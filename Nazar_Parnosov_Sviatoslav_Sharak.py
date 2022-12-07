245d7891ae64f6212f3bc86562c7ba22fc054e57
Nazarious <116711001+Nazarious-ucu@users.noreply.github.com>
2022-12-07 23:15:07 +0200

    create empte file for lab

---
+++ Nazar_Parnosov_Sviatoslav_Sharak.py
@@ -0,0 +1,84 @@
+"""
+member1: Nazar Parnosov
+member2: Sviat Sharak
+"""
+from typing import List, Dict
+
+
+def read_csv(file_name: str) -> Dict[int, List[int]]:
+    """
+    read graph represented as matrix in .csv file and return it
+    as dictionary where each key represents a vertex, while the value
+    represent the list of matrices adjacent to the key
+    :rtype: dict(key=int, value=list(int))
+    :param file_name: string
+    :return: graph
+    """
+    dct_result = {}
+
+    # read scv file and create a list of all list of list elements in lines
+    with open(file_name, 'r', encoding='utf-8') as matrix:
+        lst_lines = [line.strip().split(',') for line in matrix.readlines()]
+
+    # create keys with value (= empty list) of graph vertex in result dict
+    [dct_result.update({i: []}) for i in range(1, len(lst_lines) + 1)]
+
+    # fill in dict 
+    for i, line in enumerate(lst_lines):
+        for k, element in enumerate(line):
+            if element == '1':
+                dct_result[i + 1].append(k + 1)
+
+    return dct_result
+
+def bfs(graph: Dict[int, List[int]]) -> List[int]:
+    """
+    perform bfs on the graph and store its result
+    in the list of vertices(integers that represent vertices)
+    :rtype: list(int)
+    :param graph: dict(key=int, value=list(int))
+    :return: bfs-result
+    """
+    # Your code goes here(delete "pass" keyword)
+    pass
+
+
+def dfs(graph: Dict[int, List[int]]) -> List[int]:
+    """
+    perform dfs on the graph and store its result
+    in the list of vertices(integers that represent vertices)
+    :rtype: list(int)
+    :param graph:  dict(key=int, value=list(int))
+    :return: dfs-result
+    """
+    # Your code goes here(delete "pass" keyword)
+    pass
+
+
+def calc_pow(graph: Dict[int, List[int]]) -> Dict[int, int]:
+    """
+    calculate power of every vertex of your graph(i.e. number adjacent edges)
+    :rtype: dict(key=int, value=int)
+    :param graph: dict(key=int, value=list(int))
+    :return: vertices and their powers
+    """
+    # Your code goes here(delete "pass" keyword)
+    pass
+
+
+def find_path(n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    """
+    here is another way of representing a graph:
+    edges - is a list of edges of a graph,
+    where each edge is also a list of two integers,
+    which represent 2 adjacent vertices
+    find if there is a way from the source vertex to the destination one
+    :rtype: bool
+    :param n: int
+    :param edges: list(list(int))
+    :param source: int
+    :param destination: int
+    :return:
+    """
+    # Your code goes here(delete "pass" keyword)
+    pass
